b86199b098f6894302f7b500e7ef4b94c6ddef8c
TWer <twer@YXL.local>
2015-09-17 17:49:06 +0800

    Initial Commit

---
+++ WebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebView
+//
+//  Created by TWer  on 9/17/15.
+//  Copyright (c) 2015 TWer . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebView
+//
+//  Created by TWer  on 9/17/15.
+//  Copyright (c) 2015 TWer . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
